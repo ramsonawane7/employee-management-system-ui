bf871617225851a17d6725ea7558f3dc70e9c539
pro0833 <pksutar7588@gmail.com>
2025-11-01 23:08:47 +0530

    added public ip to api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:9090/api',
-  apiBaseUrl1: 'http://localhost:8585/api'
+  apiBaseUrl: 'http://13.232.150.115:9090/api',
+  apiBaseUrl1: 'http://13.232.150.115:8585/api'
 };
